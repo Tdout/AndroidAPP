5235d3ed986e968d4d29a32224af0e6606e46fff
Shent17 <shent17@mails.tsinghua.edu.cn>
2020-06-18 20:11:39 +0800

    Login and Register

--- AndroidBase/app/src/main/java/com/MobileCourse/LoginActivity.java
+++ AndroidBase/app/src/main/java/com/MobileCourse/LoginActivity.java
@@ -9,10 +9,19 @@ import android.view.View;
 import android.text.TextUtils;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.RadioButton;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.MobileCourse.utils.MD5Utils;
+
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 
 public class LoginActivity extends AppCompatActivity  {
@@ -21,6 +30,9 @@ public class LoginActivity extends AppCompatActivity  {
     private Button btn_login;//登录按钮
     private String userName,psw,spPsw;//获取的用户名，密码，加密密码
     private EditText et_user_name,et_psw;//编辑框
+    private RadioButton login_teacher, login_student;
+    String result = " ";
+    private  String LoginAddress = "http://47.99.112.121/login";
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
@@ -41,6 +53,8 @@ public class LoginActivity extends AppCompatActivity  {
         btn_login=findViewById(R.id.btn_login);
         et_user_name=findViewById(R.id.et_user_name);
         et_psw=findViewById(R.id.et_psw);
+        login_student = findViewById(R.id.login_student);
+        login_teacher = findViewById(R.id.login_teacher);
         //返回键的点击事件
         tv_back.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -73,95 +87,145 @@ public class LoginActivity extends AppCompatActivity  {
                 userName=et_user_name.getText().toString().trim();
                 psw=et_psw.getText().toString().trim();
                 //对当前用户输入的密码进行MD5加密再进行比对判断, MD5Utils.md5( ); psw 进行加密判断是否一致
-                String md5Psw= MD5Utils.md5(psw);
+                //String md5Psw= MD5Utils.md5(psw);
                 // md5Psw ; spPsw 为 根据从SharedPreferences中用户名读取密码
                 // 定义方法 readPsw为了读取用户名，得到密码
-                spPsw=readPsw(userName);
+                //spPsw=readPsw(userName);
+
                 // TextUtils.isEmpty
                 if(TextUtils.isEmpty(userName)){
                     Toast.makeText(LoginActivity.this, "请输入用户名", Toast.LENGTH_SHORT).show();
                     return;
-                }else if(TextUtils.isEmpty(psw)){
+                }else if (TextUtils.isEmpty(psw)){
                     Toast.makeText(LoginActivity.this, "请输入密码", Toast.LENGTH_SHORT).show();
                     return;
                     // md5Psw.equals(); 判断，输入的密码加密后，是否与保存在SharedPreferences中一致
-                }else if(md5Psw.equals(spPsw)){
-                    //一致登录成功
+                }else if(!login_student.isChecked() && !login_teacher.isChecked()){
+                    Toast.makeText(LoginActivity.this, "请选择身份", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+//                }else if((spPsw!=null&&!TextUtils.isEmpty(spPsw)&&!md5Psw.equals(spPsw))){
+//                    Toast.makeText(LoginActivity.this, "输入的用户名和密码不一致", Toast.LENGTH_SHORT).show();
+//                    return;
+//                }else{
+//                    Toast.makeText(LoginActivity.this, "此用户名不存在", Toast.LENGTH_SHORT).show();
+//                }
+                new Thread(new Runnable() {
+                    @Override
+                    public void run() {
+                        try {
+                            BufferedReader reader = null;
+                            JSONObject userJSON = new JSONObject();
+                            userJSON.put("user_id", userName);
+                            userJSON.put("password", psw);
+                            if (login_teacher.isChecked()) {
+                                userJSON.put("identity", 1);
+                            } else if (login_student.isChecked()) {
+                                userJSON.put("identity", 2);
+                            }
+                            String content = String.valueOf(userJSON);
+                            HttpURLConnection connection = (HttpURLConnection) new URL(LoginAddress).openConnection();
+                            connection.setConnectTimeout(5000);
+                            connection.setRequestMethod("POST");
+                            connection.setDoOutput(true);
+                            connection.setDoInput(true);
+                            connection.setRequestProperty("Connection", "Keep-Alive");
+                            connection.setRequestProperty("Content-Type", "application/json");
+                            connection.setRequestProperty("Charset", "UTF-8");
+                            connection.setRequestProperty("accept", "application/json");
+                            if (content != null && !TextUtils.isEmpty(content)) {
+                                byte[] writebytes = content.getBytes();
+                                connection.setRequestProperty("Content-Length", String.valueOf(writebytes.length));
+                                OutputStream os = connection.getOutputStream();
+                                os.write(content.getBytes());
+                                os.flush();
+                                os.close();
+                            }
+                            if (connection.getResponseCode() == 200) {
+                                reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+                                result = reader.readLine();
+                                System.out.println(result);
+                            }
+                        } catch (Exception e) {
+                            System.out.println("failed");
+                        }
+                    }
+                }).start();
+                //一致登录成功
+                if (Integer.valueOf(result) == 1) {
                     Toast.makeText(LoginActivity.this, "登录成功", Toast.LENGTH_SHORT).show();
                     //保存登录状态，在界面保存登录的用户名 定义个方法 saveLoginStatus boolean 状态 , userName 用户名;
-                    saveLoginStatus(true, userName);
+                    //saveLoginStatus(true, userName);
                     //登录成功后关闭此页面进入主页
-                    Intent data=new Intent();
+                    //Intent data = new Intent();
                     //datad.putExtra( ); name , value ;
-                    data.putExtra("isLogin",true);
+                    //data.putExtra("isLogin", true);
                     //RESULT_OK为Activity系统常量，状态码为-1
                     // 表示此页面下的内容操作成功将data返回到上一页面，如果是用back返回过去的则不存在用setResult传递data值
-                    setResult(RESULT_OK,data);
+                    //setResult(RESULT_OK, data);
                     //销毁登录界面
                     LoginActivity.this.finish();
                     //跳转到主界面，登录成功的状态传递到 MainActivity 中
                     startActivity(new Intent(LoginActivity.this, MainActivity.class));
                     return;
-                }else if((spPsw!=null&&!TextUtils.isEmpty(spPsw)&&!md5Psw.equals(spPsw))){
-                    Toast.makeText(LoginActivity.this, "输入的用户名和密码不一致", Toast.LENGTH_SHORT).show();
-                    return;
-                }else{
-                    Toast.makeText(LoginActivity.this, "此用户名不存在", Toast.LENGTH_SHORT).show();
+                }
+                else{
+                    Toast.makeText(LoginActivity.this, "登录失败", Toast.LENGTH_SHORT).show();
                 }
             }
         });
     }
-    /**
-     *从SharedPreferences中根据用户名读取密码
-     */
-    private String readPsw(String userName){
-        //getSharedPreferences("loginInfo",MODE_PRIVATE);
-        //"loginInfo",mode_private; MODE_PRIVATE表示可以继续写入
-        SharedPreferences sp=getSharedPreferences("loginInfo", MODE_PRIVATE);
-        //sp.getString() userName, "";
-        return sp.getString(userName , "");
-    }
-    /**
-     *保存登录状态和登录用户名到SharedPreferences中
-     */
-    private void saveLoginStatus(boolean status,String userName){
-        //saveLoginStatus(true, userName);
-        //loginInfo表示文件名  SharedPreferences sp=getSharedPreferences("loginInfo", MODE_PRIVATE);
-        SharedPreferences sp=getSharedPreferences("loginInfo", MODE_PRIVATE);
-        //获取编辑器
-        SharedPreferences.Editor editor=sp.edit();
-        //存入boolean类型的登录状态
-        editor.putBoolean("isLogin", status);
-        //存入登录状态时的用户名
-        editor.putString("loginUserName", userName);
-        //提交修改
-        editor.commit();
-    }
+//    /**
+//     *从SharedPreferences中根据用户名读取密码
+//     */
+//    private String readPsw(String userName){
+//        //getSharedPreferences("loginInfo",MODE_PRIVATE);
+//        //"loginInfo",mode_private; MODE_PRIVATE表示可以继续写入
+//        SharedPreferences sp=getSharedPreferences("loginInfo", MODE_PRIVATE);
+//        //sp.getString() userName, "";
+//        return sp.getString(userName , "");
+//    }
+//    /**
+//     *保存登录状态和登录用户名到SharedPreferences中
+//     */
+//    private void saveLoginStatus(boolean status,String userName){
+//        //saveLoginStatus(true, userName);
+//        //loginInfo表示文件名  SharedPreferences sp=getSharedPreferences("loginInfo", MODE_PRIVATE);
+//        SharedPreferences sp=getSharedPreferences("loginInfo", MODE_PRIVATE);
+//        //获取编辑器
+//        SharedPreferences.Editor editor=sp.edit();
+//        //存入boolean类型的登录状态
+//        editor.putBoolean("isLogin", status);
+//        //存入登录状态时的用户名
+//        editor.putString("loginUserName", userName);
+//        //提交修改
+//        editor.commit();
+//    }
     /**
      * 注册成功的数据返回至此
      * @param requestCode 请求码
      * @param resultCode 结果码
      * @param data 数据
      */
-    @Override
+    //@Override
     //显示数据， onActivityResult
     //startActivityForResult(intent, 1); 从注册界面中获取数据
     //int requestCode , int resultCode , Intent data
     // LoginActivity -> startActivityForResult -> onActivityResult();
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        //super.onActivityResult(requestCode, resultCode, data);
-        super.onActivityResult(requestCode, resultCode, data);
-        if(data!=null){
-            //是获取注册界面回传过来的用户名
-            // getExtra().getString("***");
-            String userName=data.getStringExtra("userName");
-            if(!TextUtils.isEmpty(userName)){
-                //设置用户名到 et_user_name 控件
-                et_user_name.setText(userName);
-                //et_user_name控件的setSelection()方法来设置光标位置
-                et_user_name.setSelection(userName.length());
-            }
-        }
-    }
+//    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+//        //super.onActivityResult(requestCode, resultCode, data);
+//        super.onActivityResult(requestCode, resultCode, data);
+//        if(data!=null){
+//            //是获取注册界面回传过来的用户名
+//            // getExtra().getString("***");
+//            String userName=data.getStringExtra("userName");
+//            if(!TextUtils.isEmpty(userName)){
+//                //设置用户名到 et_user_name 控件
+//                et_user_name.setText(userName);
+//                //et_user_name控件的setSelection()方法来设置光标位置
+//                et_user_name.setSelection(userName.length());
+//            }
+//        }
+//    }
 
 }
