7a0687eb413fc9e05d93defeaf2d581b33cafdaf
Tdout <45781359+Tdout@users.noreply.github.com>
2020-06-18 23:41:30 +0800

    6-18

--- AndroidBase/app/src/main/java/com/MobileCourse/FalseWechat/ShowMorePage.java
+++ AndroidBase/app/src/main/java/com/MobileCourse/FalseWechat/ShowMorePage.java
@@ -3,6 +3,7 @@ package com.MobileCourse.FalseWechat;
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
@@ -10,6 +11,8 @@ import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -17,39 +20,69 @@ import com.MobileCourse.Fragments.Fragment1;
 import com.MobileCourse.R;
 import com.MobileCourse.utils.CommonInterface;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class ShowMorePage extends Activity implements View.OnClickListener {
 
     //@BindView(R.id.img_social_circle)ImageView social_circle;
+    private TextView name;
+    private TextView id;
+
+    private TextView test;
+    private ArrayList<String> Exp;
+    private ArrayList<String> Res;
+    private ArrayList<String> Skill;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_wechat);
+        setContentView(R.layout.show_more_page);
         String TargetID = getID();
+        name =(TextView) findViewById(R.id.show_name);
+        id =(TextView) findViewById(R.id.show_ID);
+        id.setText("ID: " + TargetID);
+        ArrayList<HashMap<String, String>> list = new ArrayList<HashMap<String, String>>();
         // 网络请求
+        for(int i=0; i<3;i++)
+        {
+            Exp = new ArrayList<>();
+            Exp.add("EXP: " + i);
+        }
+        for(int i=0;i<40;i++){
+            //定义一个临时的hashMap
+            HashMap<String,String> hashMap = new HashMap<String, String>();
+            hashMap.put("Exp","user_"+ Exp.get(i));
+            //讲hashMap存入List
+            list.add(hashMap);
+        }
+
 
         ButterKnife.bind(this);
 
-        CommonInterface.addViewsListener(this, new int[]{
-                R.id.research_dir,
-                R.id.skill,
-                R.id.exp
-            }, this);
+//        CommonInterface.addViewsListener(this, new int[]{
+//                R.id.research_dir,
+//                R.id.skill,
+//                R.id.exp
+//            }, this);
+        SimpleAdapter listAdpter = new SimpleAdapter(
+                this,
+                list,
+                R.layout.show_more_list,
+                new String[] {"List_INFO"},
+                new int[] {R.id.list_info}
+        );
 
-//        Animation rotate = AnimationUtils.loadAnimation(this, R.anim.anim_rotate);
-//        if (rotate != null) {
-//            social_circle.startAnimation(rotate);
-//        }  else {
-//            social_circle.setAnimation(rotate);
-//            social_circle.startAnimation(rotate);
-//        }
+
     }
 
 
     @Override
     public void onClick(View v) {
         Log.e("id", v.getId() + "");
-        Toast.makeText(this,v.getId()+"",Toast.LENGTH_SHORT).show();
+        Toast.makeText(this, "id" + v.getId()+"",Toast.LENGTH_SHORT).show();
     }
     public String getID() {
         Intent intent = getIntent();
@@ -57,7 +90,35 @@ public class ShowMorePage extends Activity implements View.OnClickListener {
         //TextView showMessage = findViewById(R.id.showmessage);
         //showMessage.setText(message);
         System.out.println(message);
-        Toast.makeText(this,message,Toast.LENGTH_SHORT).show();
         return message;
     }
+
+    public List<Map<String, Object>> getDataExp(){
+        List<Map<String, Object>> list=new ArrayList<Map<String,Object>>();
+        for (int i = 0; i < Exp.size(); i++) {
+            Map<String, Object> map=new HashMap<String, Object>();
+            map.put("info", " " + Exp.get(i));
+            list.add(map);
+        }
+        return list;
+    }
+    public List<Map<String, Object>> getDataRes(){
+        List<Map<String, Object>> list=new ArrayList<Map<String,Object>>();
+        for (int i = 0; i < Res.size(); i++) {
+            Map<String, Object> map=new HashMap<String, Object>();
+            map.put("info", " " + Res.get(i));
+            list.add(map);
+        }
+        return list;
+    }
+    public List<Map<String, Object>> getDataSkill(){
+        List<Map<String, Object>> list=new ArrayList<Map<String,Object>>();
+        for (int i = 0; i < Skill.size(); i++) {
+            Map<String, Object> map=new HashMap<String, Object>();
+            map.put("info", " " + Skill.get(i));
+            list.add(map);
+        }
+        return list;
+    }
+    
 }
